b9f38430cd5dd1a317b08e4f4c4acb0171df7ead
Ilnaz Khasanov <ilnaz.hasanoff2003@gmail.com>
2022-03-11 15:09:35 +0300

    Update Program.cs

--- Matrices/Program.cs
+++ Matrices/Program.cs
@@ -1,161 +1,187 @@
-﻿using System;
-using System.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace Matrices
+namespace Others
 {
     class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Первое задание: ");
-            First();
-            Console.WriteLine("Второе задание: ");
-            Second();
-            Console.WriteLine("Третье задание: ");
-            Third();
-            Console.WriteLine("Четвертое задание: ");
-            Fourth();
+            Console.WriteLine($"C из 7 по 5 = {Combinations(7, 5)}");
+
+            //Combinations2(9, 7, out int C_9_7);
+            //Console.WriteLine($"C из 9 по 7 = {C_9_7}");
+
+            //Console.WriteLine($"ПараЛимпоМенон -> {ReverseCase("ПараЛимпоМенон")}");
+
+            //Fourth();
+            Fifth();
         }
-
-        public static void First()
+        /*
+        static int FactorialLinear(int n)
         {
-            const int height = 5, width = 7, // Количество строк и столбцов матрицы
-                minValue = 0, maxValue = 5; // Диапазон случайных чисел
-
-            Matrix m = new Matrix(height, width); // Создание матрицы
-            m.RandomFill(minValue, maxValue + 1); // Заполнение случайными числами
-            m.Print(); // Вывод на экран
-
-            // Массив для хранения количества повторений чисел, входящих в диапазон [minValue; maxValue]
-            int[] repeatings = new int[maxValue + 1]; 
-
-            int rowsWithoutZeros = 0; // Количество строк без нулевых элементов
-            bool foundZeroInRow; // Найдено ли в строке число ноль
-            for (int i = 0; i < m.height; i++)
+            int r = 1;
+            while (n > 1)
             {
-                foundZeroInRow = false;
-                for (int j = 0; j < m.width; j++)
-                {
-                    int number = m.Get(i, j); // Получаем очередное число в строке
-                    if (number == 0) foundZeroInRow = true; // Если число 0, значит эта строка содержит ноль
-                    repeatings[number]++; // Увеличиваем количество вхождений данного числа
-                }
-                if (!foundZeroInRow) rowsWithoutZeros++; // Если нулей найдено не было, тогда увеличиваем количество строк без нулевых элементов
+                r *= n--;
+            }
+            return r;
+        }
+        */
+        static int Factorial(int n)
+        {
+            if (n == 1)
+                return 1;
+
+            return n * Factorial(n - 1);
+        }
+        // First
+        static int Combinations(int n, int k)
+        {
+            return Factorial(n) / (Factorial(k) * Factorial(n - k));
+        }
+        // Second
+        static void Combinations2(int n, int k, out int result)
+        {
+            result = Factorial(n) / (Factorial(k) * Factorial(n - k));
+        }
+        static string ReverseCase(string str)
+        // Third
+        {
+            char[] letters = str.ToCharArray();
+            for (int i = 0; i < letters.Length; i++)
+            {
+                letters[i] = Char.IsLower(letters[i]) ? Char.ToUpper(letters[i]) : Char.ToLower(letters[i]);
+            }
+            return new string(letters);
+        }
+        // Fourth
+        static void Fourth()
+        {
+            const int N = 10;
+            int[] array = new int[N];
+
+            Console.WriteLine($"Массив из {N} случайных чисел от 0 до 100:");
+            Random rand = new Random();
+            for (int i = 0; i < N; i++)
+            {
+                array[i] = rand.Next(0, 100);
+                Console.WriteLine(array[i]);
             }
 
-            // Поиск максимального числа, среди повторяющихся чисел
-            int maxAmongRepeatingNumbers = minValue;
-            for (int number = minValue; number <= maxValue; number++)
-            {
-                if (repeatings[number] > 1)
-                {
-                    if (number > maxAmongRepeatingNumbers)
-                    {
-                        maxAmongRepeatingNumbers = number;
-                    }
-                }
-            }
+            CalculateArrayAverage(array, out int average);
+            CountNumbersHigherThan(array, average, out int numbersHigehrThanAverage);
 
-            // Вывод результатов на консоль
-            Console.WriteLine($"Количество строк, не содержащих ни одного нулевого элемента: {rowsWithoutZeros}");
-            Console.WriteLine($"Максимальное число, среди чисел, повторяющихся более одного раза: {maxAmongRepeatingNumbers}");
+            Console.WriteLine($"Среднее значение среди элементов массива: {average}\n" +
+                $"Количество элементов, больших среднего значения: {numbersHigehrThanAverage}");
         }
-
-        public static void Second()
+        static void CountNumbersHigherThan(int[] array, int x, out int numbersHigehrThanAverage)
         {
-            const int height = 5, width = 5, // Количество строк и столбцов матрицы
-                minValue = 0, maxValue = 10; // Диапазон случайных чисел
-
-            Matrix m = new Matrix(height, width); // Создание матрицы
-            m.RandomFill(minValue, maxValue + 1); // Заполнение случайными числами
-            m.Print();
-
-            /*
-             * 1 2 3
-             * 4 5 6
-             * 7 8 9
-             * 
-             * 1 + 4 + 7 + 5 + 8 + 9 = 34
-             */
-
-            Console.WriteLine("Сумма чисел главной диагонали и под ней данной матрицы:");
+            numbersHigehrThanAverage = 0;
+            foreach (int n in array)
+                if (n > x)
+                    numbersHigehrThanAverage++;
+        }
+        static void CalculateArrayAverage(int[] array, out int average)
+        {
             int sum = 0;
-            int numbersToSum; // Сколько чисел из данной строке нужно прибавить к общей сумме: 0 - значит 1 число, 1 - 2 числа и т.д.
-            for (int i = 0; i < m.height; i++)
+            foreach (int n in array)
             {
-                numbersToSum = i; 
-                for (int j = 0; j < m.width; j++)
-                {
-                    if (j <= numbersToSum)
-                    {
-                        sum += m.Get(i, j);
-                        Console.Write($"{m.Get(i, j)} + ");
-                    }
-                }
+                sum += n;
             }
-            Console.WriteLine($"= {sum}");
+            average = sum / array.Length;
         }
+        // Fifth
+        static void Fifth() {
+            int height = 5,
+                width = 5;
 
-        public static void Third()
-        {
-            const int height = 5, width = 6, // Количество строк и столбцов матрицы
-                minValue = 0, maxValue = 5; // Диапазон случайных чисел
+            int[,] m = new int[height, width];
 
-            Matrix m = new Matrix(height, width); // Создание матрицы
-            m.Initialize(new int[,]{ { 1, 1, 2, 5, 6, 1 }, { 5, 6, 8, 5, 6, 7 }, { 10, 12, 10, 12, 11, 11 }, { 8, 10, 5, 6, 8, 9 }, { 6, 5, 10, 12, 15, 19 } });
-            m.Print();
-
-
-            ArrayList saddlePoints = new ArrayList(); // Список для хранения узловых точек
-            int number;
-            for (int row = 0; row < m.height; row++)
+            Random rnd = new Random();
+            for (int i = 0; i < height; i++)
             {
-                for (int col = 0; col < m.width; col++)
+                for (int j = 0; j < width; j++)
                 {
-                    number = m.Get(row, col);
-                    
-                    // Если это число наименьшее в этой строке и наибольшее в этом столбце, тогда это узловая точка
-                    if (m.isMinInRow(number, row) && m.isMaxInCol(number, col))
-                    {
-                        saddlePoints.Add(new Point(row, col)); // Добавление координат числа в список узловых точек
-                    }
+                    m[i, j] = rnd.Next(10, 99);
                 }
             }
 
-            // Если найдена хотя бы одна узловая точка, тогда вывести результаты на консоль
-            if (saddlePoints.Count > 0)
+            Console.WriteLine("Исходная матрица:");
+            PrintMatrix(m);
+
+            SortMatrixColumns(m);
+
+            Console.WriteLine("Матрица после сортировки столбцов:");
+            PrintMatrix(m);
+        }
+        static void PrintMatrix(int[,] m)
+        {
+            for (int col = 0; col < m.GetLength(1); col++)
             {
-                Console.WriteLine("Координаты узловых точек матрицы (строка, столбец):");
-                foreach (Point saddlePoint in saddlePoints)
+                Console.Write($" {col} ");
+            }
+            Console.Write(" - номера столбцов\n");
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                for (int j = 0; j < m.GetLength(1); j++)
                 {
-                    Console.Write($"({saddlePoint.row}, {saddlePoint.col}) ");
+                    Console.Write($"{m[i, j]} ");
                 }
                 Console.WriteLine();
             }
-            else
+            for (int col = 0; col < m.GetLength(1); col++)
             {
-                Console.WriteLine("Матрица не имеет узловых точек.");
+                Console.Write($" {(col % 2 == 0 ? 'ч' : 'н')} ");
             }
-
+            Console.Write(" - четность столбцов\n");
         }
-
-        public static void Fourth()
+        static void SortMatrixColumns(int[,] m)
         {
-            Matrix m1 = new Matrix(2, 3); // Создание первой матрицы
-            m1.RandomFill(0, 10); // Заполнение случайными числами
-            Console.WriteLine("Первая матрица:");
-            m1.Print();
-
-            Matrix m2 = new Matrix(3, 1); // Создание второй матрицы
-            m2.RandomFill(0, 10); // Заполнение случайными числами
-            Console.WriteLine("Вторая матрица:");
-            m2.Print();
-
-            // Перемножение матриц
-            Matrix result = m1.MultiplyBy(m2);
-            Console.WriteLine("Результат перемножения матриц:");
-            result.Print();
+            int width = m.GetLength(1);
+            for (int col = 0; col < width; col++)
+            {
+                // Если столбец нечетный
+                if (col % 2 != 0)
+                {
+                    SortMatrixColumn(m, col, true); // Упорядочить по возрастанию
+                }
+                // Если столбец четный
+                else
+                {
+                    SortMatrixColumn(m, col, false); // Упорядочить по убыванию
+                }
+            }
         }
+        static void SortMatrixColumn(int[,] m, int col, bool ascending)
+        {
+            int height = m.GetLength(0);
 
+            int temp;
+            for (int i = 0; i < height - 1; i++)
+            {
+                int smallest = i;
+                for (int j = i + 1; j < height; j++)
+                {
+                    // По возрастанию
+                    if (ascending)
+                    {
+                        if (m[j, col] < m[smallest, col])
+                            smallest = j;
+                    }
+                    // По убыванию
+                    else
+                    {
+                        if (m[j, col] > m[smallest, col])
+                            smallest = j;
+                    }
+                    
+                }
+                temp = m[smallest, col];
+                m[smallest, col] = m[i, col];
+                m[i, col] = temp;
+            }
+        }
     }
 }
